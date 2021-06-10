a7e5df997a6cf8dd6c54fa3271f132d7667a2b2e
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-06-10 17:19:03 -0400

    Basic styling, log in, create account working

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,5 +1,5 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :group_name, :group_about, :group_location, :group_time, :open, :game_id, :user_id, :group_image
+  attributes :id, :group_name, :group_about, :group_location, :group_time, :open, :game_id, :user_id, :group_image, :game, :users
 
   belongs_to :game
   belongs_to :user
