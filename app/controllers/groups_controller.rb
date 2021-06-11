35817a12b4f356f47963f9670bed9052a9cb8b27
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-06-11 18:22:09 -0400

    Group info page up, group edit working, conditional button rendering working, fixed styling bugs, leave group working on DOM but needs backend persistence

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+    before_action :authorized, only: [:update]
 
     def index
         @groups = Group.all
@@ -10,4 +11,16 @@ class GroupsController < ApplicationController
         render json: @group
     end
     
+    def update
+        @group = Group.find(params[:id])
+        @group.update(group_params)
+        render json: @group
+    end
+
+    private
+
+    def group_params
+        params.permit(:group_name, :group_about, :group_location, :group_time, :open, :group_image)
+    end
+
 end
