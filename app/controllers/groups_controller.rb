649143d92532ff1051d5f08c6e21d821a2428f20
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-06-12 00:48:15 -0400

    Full CRUD on users and groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-    before_action :authorized, only: [:update]
+    before_action :authorized, only: [:update, :create, :destroy]
 
     def index
         @groups = Group.all
@@ -10,6 +10,11 @@ class GroupsController < ApplicationController
         @group = Group.find(params[:id])
         render json: @group
     end
+
+    def create 
+        @group = Group.create(group_params)
+        render json: @group
+    end
     
     def update
         @group = Group.find(params[:id])
@@ -17,10 +22,17 @@ class GroupsController < ApplicationController
         render json: @group
     end
 
+    def destroy
+        @group = Group.find(params[:id])
+        @group.memberships.destroy_all
+        @group.destroy
+        render json: {message: "Group deleted."}
+    end
+
     private
 
     def group_params
-        params.permit(:group_name, :group_about, :group_location, :group_time, :open, :group_image)
+        params.permit(:group_name, :group_about, :group_location, :group_time, :open, :group_image, :game_id, :user_id)
     end
 
 end
