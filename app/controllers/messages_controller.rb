c1c46cadf6b0f232c4be0fcd0d36eb91960d928b
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-06-15 22:07:24 -0400

    Chat works

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
     def create
         @message = Message.create(message_params)
         @conversation = Conversation.find(@message[:conversation_id])
-        ConversationChannel.broadcast_to(@conversation, @message)
+        ConversationChannel.broadcast_to(@conversation, { message: @message, user_prof_pic: @message.user.profile_pic, user_username: @message.user.username })
         render json: @message
     end
 
