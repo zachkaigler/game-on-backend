1691cb01e3686767087d3b567ed36474e73d100c
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-06-16 21:59:07 -0400

    Perfomance fix + styling

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,16 +1,16 @@
 class GameSerializer < ActiveModel::Serializer
   attributes :id, :name
 
-  has_many :users, if: condition
-  has_many :groups, if: condition
-  has_many :interests, if: condition
+  has_many :users, if: :condition
+  has_many :groups, if: :condition
+  has_many :interests, if: :condition
+
+  # has_many :users
+  # has_many :groups
+  # has_many :interests
 
   def condition
-    if @instance_options[:flag] == "restrict"
-      false
-    else
-      true
-    end
+    @instance_options[:flag] != "restrict"
   end
 
 end
